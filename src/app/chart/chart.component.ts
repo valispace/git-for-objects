79d4c39e720905e1c3852722e3a2271eb2f1a0f5
Luís Pereira <luis@valispace.com>
2022-09-30 09:59:17 +0100

    style improvements

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -23,7 +23,8 @@ export class AppChartComponent implements AfterViewInit, OnDestroy{
   ngOnInit(): void {
     this.subscriptions = [
       this.sharedService.branches$.subscribe(() => this.updateGraph()),
-      this.sharedService.commits$.subscribe(() => this.updateGraph())
+      this.sharedService.commits$.subscribe(() => this.updateGraph()),
+      this.sharedService.reqChange.subscribe(() => this.updateGraph())
     ];
   }
 
@@ -36,17 +37,32 @@ export class AppChartComponent implements AfterViewInit, OnDestroy{
   updateGraph(): void {
     const data: any[] = [
       ...this.sharedService.branches,
-      ...this.sharedService.commits
+      ...this.sharedService.commits,
     ].sort((a, b) => a.date.getTime() - b.date.getTime());
 
     this.gitgraph.clear();
     for (let obj of data) {
-      if (obj.type === 'branch') { 
-        const branch = this.gitgraph.branch(obj);
+      if (obj.type === 'branch') {
+        const branch = this.gitgraph.branch({
+          name: obj.name,
+          style: {
+            color: this.sharedService.currBranch.id === obj.id? '#e7961e' : '#cccccc',
+            label: {
+              color: this.sharedService.currBranch.id === obj.id? '#e7961e' : '#cccccc',
+              strokeColor: this.sharedService.currBranch.id === obj.id? '#e7961e' : '#cccccc',
+            }
+          }
+        });
         this.branches.set(obj.id, branch);
       } else {
         const branch: any = this.branches.get(obj.branchId);
-        branch.commit(obj);
+        branch.commit({
+          ...obj,
+          style: {
+            dot: { color: this.sharedService.currBranch.id === obj.branchId? '#e7961e' : '#979797' },
+            message: { color: this.sharedService.currBranch.id === obj.branchId? '#e7961e' : '#979797' }
+          }
+        });
       }
     }
   }
